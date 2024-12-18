96a8f3f008ff879aff1f3cc617cfb4b8d1c7a5de
esteban ortiz <ematiasortiz@gmail.com>
2024-12-18 13:22:57 -0300

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import { store } from './state/store';
 import Navigation from './navigation';
 
-const App = () => <Navigation />;
+const App: React.FC = () => (
+  <Provider store={store}>
+    <Navigation />
+  </Provider>
+);
 
 export default App;
